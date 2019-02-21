1652a32fd69af63c10dacda4d07f4aad87f05b05
Kamjar Gerami <kami.gerami@gmail.com>
2019-02-21 13:21:14 +0100

    initial dummy commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { UV_UDP_REUSEADDR } from "constants";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            item: [],
+            isLoaded: false
+        }
+    }
+
+    componentDidMount() {
+        this.getDataFromApi();
+    }
+
+    getDataFromApi = () => {
+        fetch("https://ywdi37qne9.execute-api.eu-north-1.amazonaws.com/api/user/U2FGC795G")
+        .then(response => response.json())
+        .then(data => {
+            this.setState({ 
+                items: data,
+                isLoaded:true,
+         })
+    });
+    }
+
+    render() {
+        var { isLoaded, items} = this.state;
+
+    if (!isLoaded) {
+        return <div>...Loading...</div>;
+
+    }
+    else {
+        return (
+            <div className="App">
+
+          <table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">UserName</th>
+      <th scope="col">Id</th>
+      <th scope="col">Reason</th>
+      <th scope="col">Hours</th>
+      <th scope="col">Date</th>
+
+    </tr>
+  </thead>
+  <tbody>
+      {items.map(item => (
+    <tr>
+      <th scope="row"></th>
+      <td>{item.user_name}</td>
+      <td>{item.user_id}</td>
+      <td>{item.reason}</td>
+      <td>{item.hours}</td>
+      <td>{item.event_date}</td>
+    </tr>
+      ))};
+  </tbody>
+</table>  
+            </div>
+        );
+    }
+    }
 }
 
 export default App;
